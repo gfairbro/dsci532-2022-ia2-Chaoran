d065e49d6263e724e4a975f314de84da865823f2
Chaoran Wang <showcy@vt.edu>
2022-03-08 18:34:16 -0800

    Failed to debug, restore all stuffs for now.

--- app.R
+++ app.R
@@ -7,11 +7,11 @@ library(ggplot2)
 library(plotly)
 
 # Data (wrangled)
-# raw_trees <- readr::read_csv(here::here('data', 'processed_trees.csv'))
-# raw_trees$BLOOM_START <- as.Date(raw_trees$BLOOM_START, format = "%d /%m /%Y")
-# raw_trees$BLOOM_END <- as.Date(raw_trees$BLOOM_END, format = "%d /%m /%Y")
-# raw_trees$CULTIVAR_NAME <- str_to_title(raw_trees$CULTIVAR_NAME)
-# raw_trees$COMMON_NAME <- str_to_title(raw_trees$COMMON_NAME)
+raw_trees <- readr::read_csv(here::here('data', 'processed_trees.csv'))
+raw_trees$BLOOM_START <- as.Date(raw_trees$BLOOM_START, format = "%d /%m /%Y")
+raw_trees$BLOOM_END <- as.Date(raw_trees$BLOOM_END, format = "%d /%m /%Y")
+raw_trees$CULTIVAR_NAME <- str_to_title(raw_trees$CULTIVAR_NAME)
+raw_trees$COMMON_NAME <- str_to_title(raw_trees$COMMON_NAME)
 
 # Setup app and layout/frontend
 app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
@@ -38,58 +38,58 @@ navbar <- dbcNavbarSimple(
 )
 
 # Set option list for menu filters
-# option_indicator <- function(available_indicators) {
-#   lapply(
-#     available_indicators,
-#     function(available_indicator) {
-#       list(
-#         label = available_indicator,
-#         value = available_indicator
-#       )
-#     }
-#   )
-# }
-# 
-# # Menu filters
-# date_picker <- dccDatePickerRange(
-#   id = "picker_date",
-#   start_date = as.Date("01/01/2022", format = "%d /%m /%Y"),
-#   end_date = as.Date("30/05/2022", format = "%d /%m /%Y"),
-#   min_date_allowed = as.Date("01/01/2022", format = "%d /%m /%Y"),
-#   max_date_allowed = as.Date("30/05/2022", format = "%d /%m /%Y"),
-#   start_date_placeholder_text = "Start date",
-#   end_date_placeholder_text = "End date",
-# )
-# 
-# drop_hood <- dccDropdown(
-#   id = "filter_neighbourhood",
-#   value = "all_neighbourhoods",
-#   options = c(
-#     list(list(label = "All neighbourhoods", value = "all_neighbourhoods")),
-#     option_indicator(unique(raw_trees$NEIGHBOURHOOD_NAME))
-#   )
-# )
-# 
-# drop_cultivar <- dccDropdown(
-#   id = "filter_cultivar",
-#   value = "all_cultivars",
-#   options = c(
-#     list(list(label = "All cultivars", value = "all_cultivars")),
-#     option_indicator(unique(raw_trees$CULTIVAR_NAME))
-#   )
-# )
-# 
-# range_slider <- dccRangeSlider(
-#   id = "slider_diameter",
-#   min = 0,
-#   max = 150,
-#   value = list(0, 100),
-#   marks = list(
-#     "0" = "0cm",
-#     "150" = "150cm"
-#   ),
-#   tooltip = list(placement = "bottom", always_visible = TRUE),
-# )
+option_indicator <- function(available_indicators) {
+  lapply(
+    available_indicators,
+    function(available_indicator) {
+      list(
+        label = available_indicator,
+        value = available_indicator
+      )
+    }
+  )
+}
+
+# Menu filters
+date_picker <- dccDatePickerRange(
+  id = "picker_date",
+  start_date = as.Date("01/01/2022", format = "%d /%m /%Y"),
+  end_date = as.Date("30/05/2022", format = "%d /%m /%Y"),
+  min_date_allowed = as.Date("01/01/2022", format = "%d /%m /%Y"),
+  max_date_allowed = as.Date("30/05/2022", format = "%d /%m /%Y"),
+  start_date_placeholder_text = "Start date",
+  end_date_placeholder_text = "End date",
+)
+
+drop_hood <- dccDropdown(
+  id = "filter_neighbourhood",
+  value = "all_neighbourhoods",
+  options = c(
+    list(list(label = "All neighbourhoods", value = "all_neighbourhoods")),
+    option_indicator(unique(raw_trees$NEIGHBOURHOOD_NAME))
+  )
+)
+
+drop_cultivar <- dccDropdown(
+  id = "filter_cultivar",
+  value = "all_cultivars",
+  options = c(
+    list(list(label = "All cultivars", value = "all_cultivars")),
+    option_indicator(unique(raw_trees$CULTIVAR_NAME))
+  )
+)
+
+range_slider <- dccRangeSlider(
+  id = "slider_diameter",
+  min = 0,
+  max = 150,
+  value = list(0, 100),
+  marks = list(
+    "0" = "0cm",
+    "150" = "150cm"
+  ),
+  tooltip = list(placement = "bottom", always_visible = TRUE),
+)
 
 app$layout(
   dbcContainer(
@@ -153,68 +153,68 @@ app$layout(
           id = "header"
         ),
         id = "header-back"
-      )#,
-      # # Navigation Background
-      # dbcContainer(
-      #   list(
-      #     # Menu bar
-      #     dbcContainer(
-      #       list(
-      #         dbcRow(
-      #           list(
-      #             dbcCol(
-      #               list(
-      #                 htmlLabel("Bloom date"),
-      #                 date_picker
-      #               ),
-      #               width = 3
-      #             ),
-      #             dbcCol(
-      #               list(
-      #                 htmlLabel("Neighbourhood"),
-      #                 drop_hood
-      #               ),
-      #               width = 3
-      #             ),
-      #             dbcCol(
-      #               list(
-      #                 htmlLabel("Tree cultivar (type)"),
-      #                 drop_cultivar
-      #               ),
-      #               width = 3
-      #             ),
-      #             dbcCol(
-      #               list(
-      #                 htmlLabel("Tree diameter"),
-      #                 range_slider
-      #               ),
-      #               width = 3
-      #             )
-      #           ),
-      #           id = "menu-bar"
-      #         )
-      #       )
-      #     )
-      #   ),
-      #   id = "nav-back"
-      # )#,
+      ),
+      # Navigation Background
+      dbcContainer(
+        list(
+          # Menu bar
+          dbcContainer(
+            list(
+              dbcRow(
+                list(
+                  dbcCol(
+                    list(
+                      htmlLabel("Bloom date"),
+                      date_picker
+                    ),
+                    width = 3
+                  ),
+                  dbcCol(
+                    list(
+                      htmlLabel("Neighbourhood"),
+                      drop_hood
+                    ),
+                    width = 3
+                  ),
+                  dbcCol(
+                    list(
+                      htmlLabel("Tree cultivar (type)"),
+                      drop_cultivar
+                    ),
+                    width = 3
+                  ),
+                  dbcCol(
+                    list(
+                      htmlLabel("Tree diameter"),
+                      range_slider
+                    ),
+                    width = 3
+                  )
+                ),
+                id = "menu-bar"
+              )
+            )
+          )
+        ),
+        id = "nav-back"
+      ),
       # Charts
-      # dbcContainer(
-      #   list(
-      #     dbcRow(
-      #       list(
-      #         dbcCol(
-      #           list(
-      #             htmlLabel("Blooming timeline"),
-      #             dccGraph(
-      #               id = "timeline"
-      #             )
-      #           )
-      #         )
-      #       )
-      #     )
-      #   )
-      # )
+      dbcContainer(
+        list(
+          dbcRow(
+            list(
+              dbcCol(
+                list(
+                  htmlLabel("Blooming timeline"),
+                  dccGraph(
+                    id = "timeline"
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
     ),
     id = "content"
   )
@@ -222,81 +222,81 @@ app$layout(
 
 # Chart function
 
-# timeline_plot <- function(trees_timeline) {
-#   trees_timeline <- trees_timeline %>%
-#     filter_at(vars(BLOOM_START, BLOOM_END), any_vars(!is.na(.))) %>%
-#     distinct(CULTIVAR_NAME, .keep_all = TRUE) %>%
-#     select(CULTIVAR_NAME, BLOOM_START, BLOOM_END)
-# 
-#   trees_timeline <- trees_timeline %>%
-#     gather(key = date_type, value = date, -CULTIVAR_NAME) %>%
-#     inner_join(trees_timeline, by = "CULTIVAR_NAME") %>%
-#     rename(Start = BLOOM_START, End = BLOOM_END)
-# 
-#   p <- ggplot() +
-#     geom_line(data = trees_timeline, aes(x = date, y = reorder(CULTIVAR_NAME, -as.numeric(Start)), size = "blue", color = "blue")) +
-#     scale_color_manual(values = c("#F3B2D2")) +
-#     scale_x_date(position = "top") +
-#     theme_classic() +
-#     theme(legend.position = "none") +
-#     theme(
-#       axis.title.x = element_blank(),
-#       axis.title.y = element_blank(),
-#       axis.line=element_blank()
-#     )
-# 
-#   p <- ggplotly(p + aes(Start = Start, End = End), tooltip = c("Start", "End")) %>% 
-#     layout(xaxis = list(side = "top"))
-# 
-#   return(p)
-# }
+timeline_plot <- function(trees_timeline) {
+  trees_timeline <- trees_timeline %>%
+    filter_at(vars(BLOOM_START, BLOOM_END), any_vars(!is.na(.))) %>%
+    distinct(CULTIVAR_NAME, .keep_all = TRUE) %>%
+    select(CULTIVAR_NAME, BLOOM_START, BLOOM_END)
 
-# app$callback(
-#   output("timeline", "figure"),
-#   list(
-#     input("picker_date", "start_date"),
-#     input("picker_date", "end_date"),
-#     input("filter_cultivar", "value")
-#   ),
-#   function(start_date, end_date, cultivar) {
-#     # Date input Cleanup
-#     
-#     # 
-#     # if (start_date == "None") {
-#     #   start_date <- "2022-01-01"
-#     # }
-#     #
-#     # if (end_date == "None") {
-#     #   end_date <- "2022-05-30"
-#     # }
-# 
-#     # start_date <- as.Date(start_date)
-#     # end_date <- as.Date(end_date)
-# 
-#     filtered_trees <- raw_trees
-# 
-#     # Filter by date
-# 
-#     filtered_trees <- filtered_trees %>%
-#       filter(
-#         ((BLOOM_START <= start_date) & (BLOOM_END >= start_date)) |
-#           ((BLOOM_START <= end_date) & (BLOOM_END >= end_date)) |
-#           ((BLOOM_START <= end_date) & (BLOOM_START >= start_date)) |
-#           ((BLOOM_END <= end_date) & (BLOOM_END >= start_date))
-#       )
-# 
-#     # Filter by cultivar
-#     
-#     if (cultivar != "all_cultivars") {
-#       filtered_trees <- filtered_trees %>%
-#         filter(CULTIVAR_NAME == cultivar)
-#     }
-# 
-#     timeline <- timeline_plot(filtered_trees)
-# 
-#     return(timeline)
-#   }
-# )
+  trees_timeline <- trees_timeline %>%
+    gather(key = date_type, value = date, -CULTIVAR_NAME) %>%
+    inner_join(trees_timeline, by = "CULTIVAR_NAME") %>%
+    rename(Start = BLOOM_START, End = BLOOM_END)
+
+  p <- ggplot() +
+    geom_line(data = trees_timeline, aes(x = date, y = reorder(CULTIVAR_NAME, -as.numeric(Start)), size = "blue", color = "blue")) +
+    scale_color_manual(values = c("#F3B2D2")) +
+    scale_x_date(position = "top") +
+    theme_classic() +
+    theme(legend.position = "none") +
+    theme(
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      axis.line=element_blank()
+    )
+
+  p <- ggplotly(p + aes(Start = Start, End = End), tooltip = c("Start", "End")) %>%
+    layout(xaxis = list(side = "top"))
+
+  return(p)
+}
+
+app$callback(
+  output("timeline", "figure"),
+  list(
+    input("picker_date", "start_date"),
+    input("picker_date", "end_date"),
+    input("filter_cultivar", "value")
+  ),
+  function(start_date, end_date, cultivar) {
+    # Date input Cleanup
+
+    #
+    # if (start_date == "None") {
+    #   start_date <- "2022-01-01"
+    # }
+    #
+    # if (end_date == "None") {
+    #   end_date <- "2022-05-30"
+    # }
+
+    # start_date <- as.Date(start_date)
+    # end_date <- as.Date(end_date)
+
+    filtered_trees <- raw_trees
+
+    # Filter by date
+
+    filtered_trees <- filtered_trees %>%
+      filter(
+        ((BLOOM_START <= start_date) & (BLOOM_END >= start_date)) |
+          ((BLOOM_START <= end_date) & (BLOOM_END >= end_date)) |
+          ((BLOOM_START <= end_date) & (BLOOM_START >= start_date)) |
+          ((BLOOM_END <= end_date) & (BLOOM_END >= start_date))
+      )
+
+    # Filter by cultivar
+
+    if (cultivar != "all_cultivars") {
+      filtered_trees <- filtered_trees %>%
+        filter(CULTIVAR_NAME == cultivar)
+    }
+
+    timeline <- timeline_plot(filtered_trees)
+
+    return(timeline)
+  }
+)
 
 app$callback(
   output("simple-toast", "is_open"),
